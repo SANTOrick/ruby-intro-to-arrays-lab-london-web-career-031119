681f330e2b4b87f2a4182b9962cd96bd91faea08
Riccardo <riccardo_london@live.it>
2019-03-13 09:37:51 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,24 @@
+def instantiate_new_array
+  arr = []
+  return arr
+end
+
+def array_with_two_elements
+  arr = ["Hello", "Rick"]
+  return arr
+end
+
+def first_element
+  taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  return taylor_swift[1]
+end
+
+def third_element
+  taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  return taylor_swift[2]
+end
+
+def last_element
+  taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  return taylor_swift.last
+end
