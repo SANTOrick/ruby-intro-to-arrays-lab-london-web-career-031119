2894e1a1183130ddccb394297e50a553b968bd97
Riccardo <riccardo_london@live.it>
2019-03-13 09:41:30 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -15,7 +15,7 @@ end
 
 def third_element
   taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
-  return taylor_swift[2]
+  return taylor_swift.second
 end
 
 def last_element
