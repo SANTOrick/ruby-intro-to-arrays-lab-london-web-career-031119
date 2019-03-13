5029672c0e5c7dd25bdb33ab450055fdb16f54b8
Riccardo <riccardo_london@live.it>
2019-03-13 09:42:46 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -8,12 +8,12 @@ def array_with_two_elements
   return arr
 end
 
-def first_element(1)
+def first_element(nat)
   taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
   return taylor_swift.first
 end
 
-def third_element(3)
+def third_element(not)
   taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
   return taylor_swift.third
 end
