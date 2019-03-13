cbc8dc3876032b2002a86c12dac07cf603db112a
Riccardo <riccardo_london@live.it>
2019-03-13 09:40:59 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -10,7 +10,7 @@ end
 
 def first_element
   taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
-  return taylor_swift[0]
+  return taylor_swift.first
 end
 
 def third_element
